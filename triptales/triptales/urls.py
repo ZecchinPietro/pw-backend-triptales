9f0ea44f19f1ecb334178df4f4e408616a00d4ee
MattiaFavero <9698109@itisrossi.vi.it>
2025-05-23 09:13:35 +0200

    rest

--- triptales/triptales/urls.py
+++ triptales/triptales/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
 path('backend/', include('backend.urls')),
 
 urlpatterns += [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('backend/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('backend/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
